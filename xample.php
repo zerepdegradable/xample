4e420644a46c1ec9dfef26a80d578effbdb49cd8
Zerep <perezjaldrin05@gmail.com>
2024-12-05 15:12:54 +0800

    added title and space

--- xample.php
+++ xample.php
@@ -1,5 +1,24 @@
 <!DOCTYPE html>
 <html>
+    <title>Counter Timer</title>
+    <style>
+        body{
+            font-family: 'Courier New', Courier, monospace;
+            margin-top: 100px;
+            font-size: 30px;
+            font-weight: bold;
+        }
+        h1{
+            font-weight: bold;
+            font-size: 40px;
+            font-family: 'Courier New', Courier, monospace;
+        }
+    </style>
+
+    <center>
+    <h1>Counter Timer</h1>
+    <title>Counter Timer</title>
+    <body>
     <?php
         $count = 10;
 
@@ -8,7 +27,9 @@
             $count--;
         } while ($count >= 0);
         
-        echo "Time's up!";
+        echo "<br>Time's up!";
     ?>
+    </body>
+    </center>
 </html>
 
